6a728b56db042cc090443f52296bc7f79eef5c56
Asafe Matheus <asafesousa66@outlook.com>
2021-02-07 18:55:03 -0300

    add success and failure page

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-//const request = require('request')
 const https = require('https')
 
 const app = express()
 
 app.use(express.static("public"))
 app.use(bodyParser.urlencoded({extended: true}))
-
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/signup.html')
@@ -34,8 +32,8 @@ app.post('/', (req, res) => {
     // TURN THE DATA INTO A JSON
     const jsonData = JSON.stringify(data)
 
-    const apiKey = 'your apiKey'
-    const uniqueId = 'your audience id'
+    const apiKey = 'your API key'
+    const uniqueId = 'audience id'
     const url = 'https://us7.api.mailchimp.com/3.0/lists/' + uniqueId
 
     const options = {
@@ -44,17 +42,21 @@ app.post('/', (req, res) => {
     }
 
     const request = https.request(url, options, (response) => {
-        response.on('data', (data) => {
-            console.log(JSON.parse(data))
-        })
+        if (response.statusCode == 200){
+            res.sendFile(__dirname + '/success.html')
+        } else{
+            res.sendFile(__dirname + '/failure.html')
+        }
     })
 
     request.write(jsonData)
     request.end()
 
-    res.send('ok')
 })
 
+app.post('/failure', (req, res) => {
+    res.redirect("/")
+})
 
 app.listen(3000, () => {
     console.log("Server is running on door 3000")
