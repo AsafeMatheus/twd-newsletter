a1b2502d8c9ff268c17e4e6176467554cd2c6f84
Asafe Matheus <asafesousa66@outlook.com>
2021-02-07 17:39:28 -0300

    initial commit (front-end and back-end)

---
+++ app.js
@@ -0,0 +1,61 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+//const request = require('request')
+const https = require('https')
+
+const app = express()
+
+app.use(express.static("public"))
+app.use(bodyParser.urlencoded({extended: true}))
+
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/signup.html')
+})
+app.post('/', (req, res) => {
+    const firstName = req.body.fname
+    const lastName = req.body.lname
+    const email = req.body.email
+
+    // DATA OBJECT
+    const data = {
+        members: [
+            {
+                email_address: email,
+                status: 'subscribed',
+                merge_fields: {
+                    FNAME: firstName,
+                    LNAME: lastName
+                }
+            }
+        ]
+    }
+
+    // TURN THE DATA INTO A JSON
+    const jsonData = JSON.stringify(data)
+
+    const apiKey = 'your apiKey'
+    const uniqueId = 'your audience id'
+    const url = 'https://us7.api.mailchimp.com/3.0/lists/' + uniqueId
+
+    const options = {
+        method: "POST",
+        auth: "anystring:" + apiKey
+    }
+
+    const request = https.request(url, options, (response) => {
+        response.on('data', (data) => {
+            console.log(JSON.parse(data))
+        })
+    })
+
+    request.write(jsonData)
+    request.end()
+
+    res.send('ok')
+})
+
+
+app.listen(3000, () => {
+    console.log("Server is running on door 3000")
+})
